c2330fa40e76257ce8f5177712918009bb7e47f8
nacholuquez20 <nacholuquez20@gmail.com>
2026-05-31 23:31:13 -0300

    Add barrel export for components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,21 @@
+export { Alert } from './Alert';
+export { Button } from './Button';
+export { Checkbox } from './Checkbox';
+export { ChatMensajes } from './ChatMensajes';
+export { EvidenciaGaleria } from './EvidenciaGaleria';
+export { EvidenciaUploader } from './EvidenciaUploader';
+export { FeedbackState } from './FeedbackState';
+export { ImageLightbox } from './ImageLightbox';
+export { Input } from './Input';
+export { Radio, type RadioOption } from './Radio';
+export { Select } from './Select';
+export { SeverityBadge } from './SeverityBadge';
+export { StatusBadge } from './StatusBadge';
+export { Textarea } from './Textarea';
+export {
+  DesvioHeaderActions,
+  DesvioInfoCard,
+  DesvioResponsibleCard,
+  DesvioResolutionPanel,
+  DesvioTimeline,
+} from './desvio-detail';
